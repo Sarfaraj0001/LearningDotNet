374ff7de39d3d018a280942beb34fc48d5aa4283
Sarfaraj0001 <sarfrajbabu786@gmail.com>
2024-12-23 21:41:10 +0530

    add keyup method

--- LearningDotNet/Models/User.cs
+++ LearningDotNet/Models/User.cs
@@ -19,8 +19,7 @@ namespace LearningDotNet.Models
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Mobile { get; set; }
-        public string Username { get; set; }
         public string Address { get; set; }
-        public string ProfileImage { get; set; }
+        public byte[] ProfileImage { get; set; }
     }
 }
